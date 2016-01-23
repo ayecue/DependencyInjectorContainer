977b7ffc4bf58604260c2a84204834fb2f77c400
ayecue <sorenwehmeier@Sorens-MBP.fritz.box>
2016-01-23 14:15:49 +0100

    added load by module, changed structure

--- Readme.md
+++ Readme.md
@@ -20,13 +20,13 @@ npm install dependency-injector-container
 
 Register all module defenitions to the dependency-injector-container and at the end call the load method to get everything loaded properly and in the right order.
 
-Imagine you got several modules in different files which depend on each other. You'll somehow need to compile them in the right order otherwise those modules will throw exception. With this tiny library you don't need to care about that anymore. You just need to extend all your modules to the container and then at the end call ```.load()```.
+Imagine you got several modules in different files which depend on each other. You'll somehow need to compile them in the right order otherwise those modules will throw exception. With this tiny library you don't need to care about that anymore. You just need to extend all your modules to the container and then at the end call ```.load()```. Also there's the possibility to just call ``.get()`` after all modules are extended.
 
 The pattern of this library is pretty similar to what's AngularJS is using.
 
 This library will of course only be useful if you don't already use a compiler which already cares about internal script dependencies.
 
-This library got the size of 1.4 KB.
+This library got the size of 1.6 KB.
 
 
 ## Example
@@ -58,4 +58,34 @@ Application.extend('sheep',['test'],function(test){
 });
 
 Application.load();
+```
+
+```
+var DependencyInjectorContainer = require('dependency-injector-container');
+var Application = new DependencyInjectorContainer();
+
+//Testing
+Application.extend('test',function(){
+	return {
+	  foo: 'bar'
+	};
+});
+
+Application.extend('cow',['test','sheep'],function(test,sheep){
+	console.log(test.foo, sheep);
+
+	return {
+	  lol: 'moo'
+	};
+});
+
+Application.extend('sheep',['test'],function(test){
+	console.log(test.foo);
+
+	return {
+	  legs: 'walk'
+	};
+});
+
+Application.get('cow');
 ```
--- main.js
+++ main.js
@@ -5,12 +5,13 @@
  * Copyright (c) 2015 "AyeCue" Sören Wehmeier, contributors
  * Licensed under the MIT license.
  */
-;(function(name, definition) {
+;
+(function(name, definition) {
   var theModule = definition();
   var hasDefine = typeof define === 'function' && define.amd;
   var hasExports = typeof module !== 'undefined' && module.exports;
- 
-  if (hasDefine){ // AMD Module
+
+  if (hasDefine) { // AMD Module
     define(theModule);
   } else if (hasExports) { // Node.js Module
     module.exports = theModule;
@@ -18,144 +19,256 @@
     window[name] = theModule;
   }
 })('DependencyInjectorContainer', function() {
-/**
- * Dependency injector container like in AngualarJS.
- *
- * @class DependencyInjectorContainer
- * @constructor
- * @param {Object} scope  Scope for namespaces.
- * @private
- */
+  //Error Messages
+  var NAMESPACE_TYPE_ERROR = 'Unexpected type for namespace. Namespace needs to be a string.'; //Thrown when unexpected namespace type is given
+  var NAMESPACE_ALREADY_DEFINED = '%n is aready defined.'; //Thrown as soon as someone tries to override a namespace without the overriden flag set to true
+  var NAMESPACE_NOT_FOUND = '%n not found.'; //Thrown if namespace is not set
+  var DEFINITION_NOT_INITIALIZED = 'Definition %d is not initialized.'; //Thrown if definition is not initialized
+  var DEFINITION_CYCLIC_DEPENDENCY = 'Cyclic dependency in %1 and %2.'; //Thrown if there are cyclic dependencies.
+
+  /**
+   * Dependency injector container like in AngualarJS.
+   *
+   * @class DependencyInjectorContainer
+   * @constructor
+   * @param {Object} scope  Scope for namespaces.
+   * @private
+   */
   function DependencyInjectorContainer(scope) {
-    this.scope = scope || {};
-  }
+    var hasProperty;
 
-  /**
-   * Set namespace in scope. Module will be instantly available.
-   *
-   * @method set
-   * @chainable
-   * @param {String} namespace  Module namespace.
-   * @param {Object} module  Module to set.
-   * @param {Boolean} [override]  Should override namespace if set.
-   */
-  DependencyInjectorContainer.prototype.set = function(namespace, module, override) {
-    if (typeof namespace !== 'string') {
-      throw new TypeError('Namespace has to be a string.');
+    scope = scope || {};
+
+    this.set = set;
+    this.get = get;
+    this.extend = extend;
+    this.load = load;
+
+    /**
+     * Assert helper.
+     *
+     * @method assert
+     * @private
+     * @param {Boolean} condition
+     * @param {String} message  
+     * @param {Mixed} [type]  
+     */
+    function assert(condition, message, type) {
+      if (condition === false) {
+        throw new (type || Error)(message);
+      }
     }
 
-    if (override !== true && this.scope.hasOwnProperty(namespace) === true) {
-      throw new Error(namespace + ' is aready defined.');
-    }
+    /**
+     * Set namespace in scope. Module will be instantly available.
+     *
+     * @method set
+     * @chainable
+     * @param {String} namespace  Module namespace.
+     * @param {Object} module  Module to set.
+     * @param {Boolean} [override]  Should override namespace if set.
+     */
+    function set(namespace, module, override) {
+      assert(typeof namespace === 'string', NAMESPACE_TYPE_ERROR, TypeError);
+      assert(
+        override === true || scope.hasOwnProperty(namespace) === false, 
+        NAMESPACE_ALREADY_DEFINED
+          .replace('%n', namespace)
+      );
 
-    this.scope[namespace] = module;
+      scope[namespace] = module;
 
-    return this;
-  };
+      return this;
+    };
 
-  /**
-   * Extend namespace to scope. Will be injected as soon as load is called.
-   *
-   * @method extend
-   * @chainable
-   * @param {String} namespace  Module namespace.
-   * @param {Array} [dependencies]  Module dependencies.
-   * @param {Object} definition  Module definition block.
-   */
-  DependencyInjectorContainer.prototype.extend = function(namespace, dependencies, definition) {
-    if (typeof dependencies === 'function') {
-      definition = dependencies;
-      dependencies = undefined;
-    }
+    /**
+     * Extend namespace to scope. Will be injected as soon as load is called.
+     *
+     * @method extend
+     * @chainable
+     * @param {String} namespace  Module namespace.
+     * @param {Array} [dependencies]  Module dependencies.
+     * @param {Object} definition  Module definition block.
+     */
+    function extend(namespace, dependencies, definition) {
+      if (typeof dependencies === 'function') {
+        definition = dependencies;
+        dependencies = undefined;
+      }
 
-    definition.__dependencies__ = dependencies || [];
-    definition.isDefinition = true;
-    this.set(namespace, definition);
+      definition.__dependencies__ = dependencies || [];
+      definition.isDefinition = true;
+      definition.namespace = namespace;
 
-    return this;
-  };
+      set(namespace, definition);
 
-  /**
-   * Receive module.
-   *
-   * @method get
-   * @param {String} namespace  Module namespace.
-   * @return {Object}
-   */
-  DependencyInjectorContainer.prototype.get = function(namespace) {
-    if (typeof namespace !== 'string') {
-      throw new TypeError('Namespace has to be a string.');
-    }
+      return this;
+    };
 
-    if (typeof this.scope[namespace] === 'undefined') {
-      throw new Error(namespace + ' not found.');
-    }
+    /**
+     * Receive module. Will automatically initialize module with all it dependencies if it's not already loaded.
+     *
+     * @method get
+     * @param {String} namespace  Module namespace.
+     * @return {Object}
+     */
+    function get(namespace) {
+      assert(typeof namespace === 'string', NAMESPACE_TYPE_ERROR, TypeError);
+      assert(
+        scope.hasOwnProperty(namespace) === true, 
+        NAMESPACE_NOT_FOUND
+          .replace('%n', namespace)
+      );
 
-    return this.scope[namespace];
-  };
+      return loadDefinition(scope[namespace]);
+    };
 
-  /**
-   * Initialize all defenitions.
-   *
-   * @method load
-   * @chainable
-   */
-  DependencyInjectorContainer.prototype.load = function() {
-    var namespaces = Object.keys(this.scope).filter(function(namespace){
-      return this.get(namespace).isDefinition === true;
-    }, this);
-    var sortedNamespaces = [];
-    var queue = function(collection) {
-      var result = [];
+    /**
+     * Evaluate defenition.
+     *
+     * @method evalDefinition
+     * @private
+     * @param {String} namespace  Module namespace.
+     * @return {Object}
+     */
+    function evalDefinition(definition) {
+      var args;
+      var compiled;
+      var depDefinition;
 
-      collection.forEach(function(namespace){
-        var isRequired = namespaces.some(function(m){
-          if (m !== namespace && this.get(m).__dependencies__.indexOf(namespace) !== -1) {
-            if (this.get(namespace).__dependencies__.indexOf(m) !== -1) {
-              console.error('Cyclic dependency in ' + namespace + ' and ' + m + '.');
-              return;
-            }
+      if (definition.isDefinition !== true) {
+        return definition;
+      }
 
-            return true;
-          }
-        }, this);
+      args = definition.__dependencies__.map(function(depNamespace) {
+        depDefinition = get(depNamespace);
 
-        if (isRequired === true) {
-          result.push(namespace);
-        } else {
-          sortedNamespaces.unshift(namespace);
-        }
-      }, this);
-
-      return result;
-    }.bind(this);
-
-    while (namespaces.length > 0) {
-      namespaces = queue(namespaces);
-    }
-
-    sortedNamespaces.forEach(function(namespace){
-      var definition = this.get(namespace);
-      var dependencies = definition.__dependencies__;
-      var args = dependencies.map(function(depNamespace){
-        var module = this.get(depNamespace);
-
-        if (module.isDefinition === true) {
-          console.warn('Module ' + depNamespace + ' is not initialized.');
+        if (depDefinition.isDefinition === true) {
+          console.warn(
+            DEFINITION_NOT_INITIALIZED
+            .replace('%d', depNamespace)
+          );
         }
 
-        return module;
-      }, this);
+        return depDefinition;
+      });
 
       try {
-        this.set(namespace, definition.apply(this, args) || {}, true);
+        compiled = definition.apply(null, args) ||  {};
+        set(definition.namespace, compiled, true);
       } catch (e) {
-        console.error(namespace, e);
+        console.error(definition.namespace, e);
       }
-    }, this);
 
-    return this;
-  };
+      return compiled;
+    };
+
+    /**
+     * Load one defenition with all it dependencies. (Use this if you just want to load certain modules and not all)
+     * If you want to load via namespace use .get instead.
+     *
+     * @method loadDefinition
+     * @private
+     * @param {Function} definition  Definition block.
+     * @param {Array} [excludes]  Modules to exclude from load.
+     * @return {Object}
+     */
+    function loadDefinition(definition, excludes) {
+      var sortedDefinitions = [];
+
+      if (definition.isDefinition !== true) {
+        return definition;
+      }
+
+      excludes = excludes ||  [];
+      excludes.push(definition);
+
+      definition.__dependencies__.forEach(function(depNamespace) {
+        var depDefinition = get(depNamespace);
+        var excluded;
+        var result;
+
+        if (depDefinition.isDefinition !== true) {
+          return;
+        }
+
+        if (depDefinition.__dependencies__.indexOf(definition.namespace) !== -1) {
+          console.error(
+            DEFINITION_CYCLIC_DEPENDENCY
+            .replace('%1', definition.namespace)
+            .replace('%2', depDefinition.namespace)
+          );
+          return;
+        }
+
+        excluded = excludes.some(function(d) {
+          return depDefinition.namespace !== d.namespace;
+        });
+
+        if (excluded === false) {
+          return;
+        }
+
+        result = loadDefinition(depDefinition, excludes);
+        sortedDefinitions = sortedDefinitions.concat(result);
+      });
+
+      return evalDefinition(definition);
+    };
+
+    /**
+     * Initialize all defenitions.
+     *
+     * @method load
+     * @chainable
+     */
+    function load() {
+      var namespaces = Object.keys(scope).filter(function(namespace) {
+        return get(namespace).isDefinition === true;
+      });
+      var sortedNamespaces = [];
+      var queue = function(collection) {
+        var result = [];
+
+        collection.forEach(function(namespace) {
+          var isRequired = namespaces.some(function(m) {
+            if (m !== namespace && get(m).__dependencies__.indexOf(namespace) !== -1) {
+              if (get(namespace).__dependencies__.indexOf(m) !== -1) {
+                console.error(
+                  DEFINITION_CYCLIC_DEPENDENCY
+                  .replace('%1', namespace)
+                  .replace('%2', m)
+                );
+                return;
+              }
+
+              return true;
+            }
+          });
+
+          if (isRequired === true) {
+            result.push(namespace);
+          } else {
+            sortedNamespaces.unshift(namespace);
+          }
+        });
+
+        return result;
+      };
+
+      while (namespaces.length > 0) {
+        namespaces = queue(namespaces);
+      }
+
+      sortedNamespaces.forEach(function(namespace) {
+        var definition = get(namespace);
+
+        evalDefinition(definition);
+      });
+
+      return this;
+    };
+  }
 
   return DependencyInjectorContainer;
 });
